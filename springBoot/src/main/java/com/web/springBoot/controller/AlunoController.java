e537ad51b27b674cce177ab90ab425681c9e9947
Paloma_Marian <83973947+Paloma-Marian@users.noreply.github.com>
2023-12-17 15:09:34 -0300

    Front-End Inicial

--- springBoot/src/main/java/com/web/springBoot/controller/AlunoController.java
+++ springBoot/src/main/java/com/web/springBoot/controller/AlunoController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.web.springBoot.entities.Aluno;
 import com.web.springBoot.repository.AlunoRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/alunos")
 public class AlunoController {
